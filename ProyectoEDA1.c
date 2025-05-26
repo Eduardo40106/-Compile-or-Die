a5f0196fd7e2d9f98e3a41b8824d156d202920fd
Eduardo40106 <l.eduardocuevas31@gmail.com>
2025-05-25 19:51:40 -0600

    Empieza el proyecto

---
+++ ProyectoEDA1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+
+struct adoptante{
+    char nombre[100];
+    int numTelefonico;
+    char correo[100];
+    char contraseña[100];
+    char ID[10];    
+};
+struct mascota{
+
+    char especie[50];
+
+};
