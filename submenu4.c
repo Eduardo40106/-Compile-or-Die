1943eae6f6db4948469af3bee12ffb458aa8437a
GT-8 <30545002+GT-8@users.noreply.github.com>
2025-05-28 19:14:13 -0600

    Update submenu4.c

--- submenu4.c
+++ submenu4.c
@@ -3,3 +3,27 @@
 #include <string.h>
 #include <time.h>
 #include "funcionalidades.h"
+
+void buscar_mascotas_especie_edad(struct Mascota* mascotas, char* especie, int edad_min, int edad_max) {
+	char opcion = 0;
+
+	if(mascotas != NULL && !strcmp(mascotas->especie, especie) &&
+			mascotas->edad >= edad_min && mascotas->edad <= edad_max) {
+		printf("\n=== Mascota Encontrada ===\n");
+		printf("Nombre: %s\n", mascotas->nombre);
+		printf("Edad: %d años\n", mascotas->edad);
+		printf("Descripción: %s\n", mascotas->descripcion);
+
+		printf("Ingrese \"M\" para ADOPTAR, \"Q\" para SALIR, cualquier otra letra para continuar: ");
+		scanf("%c", &opcion); getchar();
+	}
+
+	switch(opcion) {
+		case 'M':
+			mascotas->adoptada = 1;
+		case 'Q':
+			break;
+		default:
+			buscar_mascotas_especie_edad(mascotas->siguiente, especie, edad_min, edad_max);
+	}
+}
